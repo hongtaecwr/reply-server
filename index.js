93721cdca5f5f39cfe19e0e638ce1f42444a406c
hongtaecwr <t.chawarat@kkumail.com>
2018-11-08 18:47:43 +0700

    v2

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var ParseDashboard = require('parse-dashboard');
+
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
