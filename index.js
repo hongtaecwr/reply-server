314b50e443ebed9e952819e1779c79e6d3fd4415
hongtaecwr <t.chawarat@kkumail.com>
2018-11-08 21:34:44 +0700

    v7

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ var dashboard = new ParseDashboard({
 }, allowInsecureHTTP);
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://heroku_khfrwmjr:a4oejsqonpkd6sc91ga1tq2htu@ds255253.mlab.com:55253/heroku_khfrwmjr',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'WiGnYW7WsWcYqYwDQh7R',
   masterKey: process.env.MASTER_KEY || 'BtDqejjk3ABobcWW41ls', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://reply-server.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
