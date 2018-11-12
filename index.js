686f38b4cf992211376318bcf7f9753cec94b457
hongtaecwr <t.chawarat@kkumail.com>
2018-11-13 03:37:12 +0700

    v6

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (!databaseUri) {
 var trustProxy = true;
 var dashboard = new ParseDashboard({
   "apps": [{
-    "serverURL": "https://reply-msg-server.herokuapp.com/parse",
+    "serverURL": "https://reply-server.herokuapp.com/parse",
     "appId": "myAppId",
     "masterKey": "myMasterKey",
     "appName": "reply-server"
@@ -33,7 +33,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://reply-msg-server.herokuapp.com/parse', // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://reply-server.herokuapp.com/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
