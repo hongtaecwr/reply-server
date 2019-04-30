f2db83124e75b3d4977ae43a06a3d59107af5e5b
Chawarat Thongyothee <t.chawarat@kkumail.com>
2019-04-30 14:55:41 +0700

    hongtae

--- cloud/main.js
+++ cloud/main.js
@@ -363,8 +363,7 @@ Parse.Cloud.define('addSynonym', function (request, response) {
 Parse.Cloud.define('getSynonym', function (request, response) {
   var SYN = Parse.Object.extend("Synonym");
   var query = new Parse.Query(SYN);
-  var msg = "กิน";
-  query.containedIn("common_word", msg)
+  query.equalTo("common_word", "กิน")
   query.find({
     success: function (result) {
       console.log(result);
