a1ff0193616b28b5b6b2052f7c18e95040b5c68f
hongtaecwr <hongtae2007@hotmail.com>
2019-05-03 22:41:29 +0700

    Hongtae

--- cloud/main.js
+++ cloud/main.js
@@ -342,7 +342,7 @@ Parse.Cloud.define('addSynonym', function (request, response) {
 });
 
 ///////////////////////
-Parse.Cloud.define('getSynonym', function (request, response) {
+Parse.Cloud.define('getSynonym', async (request) => {
   var SYN = Parse.Object.extend("Synonym");
   var query = new Parse.Query(SYN);
   query.equalTo("common_word", "change")
