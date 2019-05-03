5a21f07b718692f93439b4186178d84adc7493d9
hongtaecwr <hongtae2007@hotmail.com>
2019-05-04 02:57:58 +0700

    Hongtae

--- cloud/main.js
+++ cloud/main.js
@@ -348,12 +348,11 @@ Parse.Cloud.define('getSynonym', function (request, response) {
       for (var i = 0; i < result.length; i++) {
         common_word = result[i].get("common_word");
         synonym_word = result[i].get("synonym_word");
-
-        response.success({
-          "common_word": common_word,
-          "synonym_word": synonym_word
-        });
       }
+        response.success({
+          "common_word": common_word[i],
+          "synonym_word": synonym_word[i]
+        }); 
     },
     error: function () {
       response.error("failed");
