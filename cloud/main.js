5902c0a020e971b5633efa2d3e680ed39e123964
hongtaecwr <hongtae2007@hotmail.com>
2019-05-06 00:02:06 +0700

    Hongtae

--- cloud/main.js
+++ cloud/main.js
@@ -60,9 +60,7 @@ function getReplyMsg(request, response, msgFromUser) {
           common_word = result[i].get("common_word");
           synonym_word = result[i].get("synonym_word");
           msgFromUser = msgFromUser.replace(new RegExp(common_word, 'g'),synonym_word);
-          console.log(msgFromUser);
         }
-        console.log(msgFromUser);
           response.success(msgFromUser); 
       },
       error: function () {
