e4b9883f89f0597d8e2f8b97ad0046e3f6571625
hongtaecwr <hongtae2007@hotmail.com>
2019-05-19 00:07:40 +0700

    Hongtae

--- cloud/main.js
+++ cloud/main.js
@@ -231,10 +231,12 @@ Parse.Cloud.define("findBestMsgFromUnknow", function (request, response) {
             });
           });
           var matches = stringSimilarity.findBestMatch(msgFromUser, msgArray);
+          var target = matches.bestMatch.target;
+          var ratings = matches.bestMatch.rating;
           console.log("matches:" + JSON.stringify(matches));
           console.log("best matches:" + JSON.stringify(matches.bestMatch));
-          var target = matches.bestMatch.target;
           console.log("result bestMatch target:" + target);
+          console.log("Ratings is " + ratings)
 
           getReplyMsg({
             params: {
