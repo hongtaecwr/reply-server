9d78f72408d8c2915e88546b5c35de26840912b8
Chawarat Thongyothee <t.chawarat@kkumail.com>
2019-05-19 11:36:26 +0700

    Hongtae

--- cloud/main.js
+++ cloud/main.js
@@ -207,7 +207,7 @@ Parse.Cloud.define("findBestMsgFromUnknow", function (request, response) {
           synonym_word = result[i].get("synonym_word");
           msgFromUser = msgFromUser.replace(new RegExp(common_word, 'g'), synonym_word);
         }
-        console.log("After Synonym : " + msgFromUser);
+        console.log("After Synonym2 : " + msgFromUser);
         var wc = wordcut.cut(msgFromUser)
         let arr = wc.split('|');
         query.containedIn("wordsArray", arr);
